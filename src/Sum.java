fe79df2b8fc904843ef4ce0889b6e6b60ead8bf4
Long Pham <ronglongbk01@gmail.com>
2021-11-08 12:09:58 +0700

    Class Sum

---
+++ src/Sum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a, b, sum;
+		System.out.println("Nhap a = ");
+		a = scanner.nextInt();
+		System.out.println("Nhap b = ");
+		b = scanner.nextInt();
+		sum = a + b;
+		System.out.println("Sum = " + sum);
+
+	}
+
+}
